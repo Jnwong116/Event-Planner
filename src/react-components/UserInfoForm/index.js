9bfdddf0e8ca505c6f8586edf792b6f13d5c4a18
Jordan <jnwong116@gmail.com>
2021-10-26 20:22:30 -0400

    Added a user info form to be used for sign up and edit profile pages

---
+++ src/react-components/UserInfoForm/index.js
@@ -0,0 +1,53 @@
+import React from "react";
+
+import './style.css';
+
+class UserInfoForm extends React.Component {
+    render() {
+        const {
+            name,
+            userName,
+            password,
+            email,
+            birthday,
+            handleChange
+        } = this.props;
+
+        return (
+            <div>
+                <input
+                value={name}
+                onChange={handleChange}
+                type="text"
+                name="name"
+                placeholder="Enter First and Last Name" />
+                <input
+                value={userName}
+                onChange={handleChange}
+                type="text"
+                name="userName"
+                placeholder="Enter UserName" />
+                <input
+                value={password}
+                onChange={handleChange}
+                type="text"
+                name="password"
+                placeholder="Enter Password" />
+                <input
+                value={email}
+                onChange={handleChange}
+                type="text"
+                name="email"
+                placeholder="Enter your email" />
+                <input
+                value={birthday}
+                onChange={handleChange}
+                type="text"
+                name="birthday"
+                placeholder="Enter your birthday" />
+            </div>
+        )
+    }
+}
+
+export default UserInfoForm;
