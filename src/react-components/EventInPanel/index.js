f81aa504b763c8472f7ad15757ca6f825aa3dcc9
Haifeng Sun <haifeng.sun@mail.utoronto.ca>
2021-11-05 17:22:12 -0400

    working on eventpanel

---
+++ src/react-components/EventInPanel/index.js
@@ -0,0 +1,17 @@
+class EventInPanel extends React.Component{
+    state = {
+        events: [
+            {id: "0", name: "eventname1"},
+            {id: "1", name: "eventname2"}
+        ]
+    }
+    render() {
+        return (
+            <div>
+                
+            </div>
+        );
+    }
+}
+
+export default EventInPanel;
