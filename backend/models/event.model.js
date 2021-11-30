49294bb515ffaf9d2d354b7fd7bf052aa06fe1d3
bhatiate <tevan.bhatia@mail.utoronto.ca>
2021-11-30 13:09:12 -0500

    initial backend setup

---
+++ backend/models/event.model.js
@@ -0,0 +1,28 @@
+const mongoose = require('mongoose')
+
+const Schema = mongoose.Schema
+
+const messageSchema = new Schema({
+    sender: {type: String, required: true},
+    content: {type: String},
+    timestamp: {type: Date, default: Date.now}
+})
+
+const taskSchema = new Schema({
+    id: {type: String, required: true},
+    user: [Number],
+    status: {type: String},
+    date: {type: Date, default: Date.now}
+})
+
+const eventSchema = new Schema({
+    id: {type: String, require: true},
+    name: {type: String, require: true},
+    style: {type: String, require: true},
+    messages: [messageSchema],
+    tasks: [taskSchema]  
+})
+
+const Event = mongoose.model('Event', eventSchema)
+
+module.exports = Event
